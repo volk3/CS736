28e96dc7ea7ef05fc501c00733a881893047b3e1
cramja <marc.spehlmann@gmail.com>
2015-11-04 08:24:20 -0800

    kmessage print

--- kernel/scallinfo.c
+++ kernel/scallinfo.c
@@ -2,8 +2,13 @@
 #include <linux/init.h>
 #include <linux/syscalls.h>
 #include <linux/linkage.h>
+#include <linux/sched.h>
 
 asmlinkage long sys_scallinfo(int scnum) {
-	printk(KERN_EMERG "service running");
-	return 0;
+	printk("called scallinfo arg: %d\n", scnum);
+    struct task_struct* ts;
+    ts = find_task_by_vpid(scnum);
+    if(ts == NULL)
+        return -1;
+    return ts->pid;
 }
